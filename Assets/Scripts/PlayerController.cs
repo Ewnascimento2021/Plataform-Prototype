681c7a4aa130134edecc4dffcc5dc89d87c665c3
Ewnascimento <ewerton_snascimento@outlook.com>
2022-05-31 11:27:00 -0300

    Walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private int moveSpeed;
+    [SerializeField]
+    private int jumpSpeed;
+
+    private float direction;
+    private Vector2 facingRight;
+    private Vector2 facingLeft;
+    
+
     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        facingRight = transform.localScale;
+        facingLeft = transform.localScale;
+        facingLeft.x = facingLeft.x * -1;
+    }
+
+    private void FixedUpdate()
+    {
+        Walking();
+
+        Jump();
+
+        Fight();
+    }
+
+    private void Fight()
     {
         
     }
 
-    // Update is called once per frame
-    void FixeUpdate()
+    private void Jump()
     {
-        
+       
+    }
+
+    private void Walking()
+    {
+        direction = (Input.GetAxis("Horizontal"));
+
+        if (direction > 0)
+        {
+            transform.localScale = facingRight;
+        }
+
+        if (direction < 0)
+        {
+            transform.localScale = facingLeft;
+        }
+
+
+
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+
+
     }
 }
