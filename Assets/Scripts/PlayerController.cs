b1927f230f40729e316092933a745fd9241f7fc6
Ewnascimento <ewerton_snascimento@outlook.com>
2022-05-31 16:32:57 -0300

    Jump and Double Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,12 +10,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private int moveSpeed;
     [SerializeField]
-    private int jumpSpeed;
+    private int doubleJump;
+    [SerializeField]
+    private float jumpSpeed;
+    [SerializeField]
+    private bool isGrounded;
 
     private float direction;
     private Vector2 facingRight;
     private Vector2 facingLeft;
-    
+
 
     void Start()
     {
@@ -30,39 +34,78 @@ public class PlayerController : MonoBehaviour
     {
         Walking();
 
-        Jump();
-
         Fight();
     }
 
-    private void Fight()
+    private void Update()
     {
-        
+        DoubleJump();
     }
 
     private void Jump()
     {
-       
+        rb.velocity = Vector2.up * jumpSpeed;
     }
+
+
+    private void DoubleJump()
+    {
+        if (isGrounded)
+        {
+            doubleJump = 1;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Jump") && doubleJump > 0)
+            {
+                doubleJump--;
+                Jump();
+            }
+        }
+    }
+
 
     private void Walking()
     {
-        direction = (Input.GetAxis("Horizontal"));
+        direction = (Input.GetAxisRaw("Horizontal"));
 
-        if (direction > 0)
+
+        if (direction == 1)
         {
             transform.localScale = facingRight;
         }
 
-        if (direction < 0)
+        else if (direction == -1)
         {
             transform.localScale = facingLeft;
         }
 
-
-
         rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+    }
 
+    private void Fight()
+    {
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
